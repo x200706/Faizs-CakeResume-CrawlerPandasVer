4aa1c2661e748035fc1ec6c789dc3d2b9112d3d7
x200706 <99391710+x200706@users.noreply.github.com>
2023-07-28 15:16:41 +0000

    URL Decode

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 from bs4 import BeautifulSoup
 import pandas as pd
 
-list_serach_job = ["後端工程師"]
+list_serach_job = ["後端工程師", "軟體工程師", "Java", "Django"]
 
 # 创建一个空的DataFrame用于存储数据
 df = pd.DataFrame(columns=['職缺名稱', '職缺連結', '公司名稱', '地區', '月薪', '給薪方式'])
@@ -16,9 +16,9 @@ for serach_job in list_serach_job:
   while page < 150:
     page += 1
     response = requests.get(
-        "https://www.cakeresume.com/jobs/" + serach_job +
-        "?location_list%5B0%5D=Taipei%20City%2C%20Taiwan&location_list%5B1%5D=New%20Taipei%20City%2C%20Taiwan&location_list%5B2%5D=%E5%8F%B0%E5%8C%97%E5%B8%82%2C%20%E5%8F%B0%E7%81%A3&location_list%5B3%5D=%E6%96%B0%E5%8C%97%E5%B8%82%2C%20%E5%8F%B0%E7%81%A3&seniority_level%5B0%5D=entry_level&seniority_level%5B1%5D=internship_level&seniority_level%5B2%5D=associate&page="
-        + str(page))  # 台北 新北 初階 實習 助理
+      "https://www.cakeresume.com/jobs/" + serach_job +
+      "?location_list[0]=Taipei City, Taiwan&location_list[1]=New Taipei City, Taiwan&location_list[2]=台北市, 台灣&location_list[3]=新北市, 台灣&seniority_level[0]=entry_level&seniority_level[1]=internship_level&seniority_level[2]=associate&page="
+      + str(page))  # 台北 新北 初階 實習 助理
     soup = BeautifulSoup(response.text, 'html.parser')
 
     try:
@@ -30,15 +30,15 @@ for serach_job in list_serach_job:
 
     for job in soup.find_all(
         'div', class_='JobSearchPage_searchResults__yLBAf')[0].find_all(
-            'div', class_='JobSearchItem_wrapper__0zoCh'):
+          'div', class_='JobSearchItem_wrapper__0zoCh'):
       job1 = job.find_all('a', 'JobSearchItem_jobTitle__Fjzv2')[0].text
       job2 = 'https://www.cakeresume.com' + job.find_all(
-          'div', class_='JobSearchItem_headerTitle__k_1FH')[0].a['href']
+        'div', class_='JobSearchItem_headerTitle__k_1FH')[0].a['href']
       job3 = job.find_all('div', 'JobSearchItem_headerSubtitle__XoiMM')[0].text
       job5 = job.find_all(
-          'div',
-          class_=
-          'InlineMessage_inlineMessage__I9C_W InlineMessage_inlineMessageLarge__yeH0A InlineMessage_inlineMessageDark__rNo_a'
+        'div',
+        class_=
+        'InlineMessage_inlineMessage__I9C_W InlineMessage_inlineMessageLarge__yeH0A InlineMessage_inlineMessageDark__rNo_a'
       )[2].text
       print(job1, job2, job3, job5)
       job51 = job5[0:job5.find('/')]
@@ -58,14 +58,14 @@ for serach_job in list_serach_job:
 
       print(low_price, high_price)
       df = df._append(
-          {
-              '職缺名稱': job1,
-              '職缺連結': job2,
-              '公司名稱': job3,
-              '地區': job5,
-              '月薪': job52
-          },
-          ignore_index=True)
+        {
+          '職缺名稱': job1,
+          '職缺連結': job2,
+          '公司名稱': job3,
+          '地區': job5,
+          '月薪': job52
+        },
+        ignore_index=True)
     print("頁數", page)
     print("***************")
     time.sleep(random.randint(5, 15))
